389da7d602828e5a1d6fc92ff2c08bf184fa55cb
Dinar Subeev <dinars163@gmail.com>
2023-07-05 10:24:02 +0400

    Создан пустой проект

---
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ggr3ml1n.shoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
