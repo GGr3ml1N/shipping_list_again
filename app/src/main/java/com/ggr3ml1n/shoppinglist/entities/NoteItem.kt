29bccc96d4939c7c7899e85e768e3a1c1ec03f0d
Dinar Subeev <dinars163@gmail.com>
2023-07-05 11:46:32 +0400

    Заполнена таблица "note_list"

--- app/src/main/java/com/ggr3ml1n/shoppinglist/entities/NoteItem.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/entities/NoteItem.kt
@@ -6,5 +6,18 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "note_list")
 data class NoteItem(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
 
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @ColumnInfo(name = "content")
+    val content: String,
+
+    @ColumnInfo(name = "time")
+    val time: String,
+
+    @ColumnInfo(name = "category")
+    val category: String,
 )
