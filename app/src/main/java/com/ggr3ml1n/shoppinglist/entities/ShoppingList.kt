a1434c4cdedb100f00de910a22ec8847f4daad42
Dinar Subeev <dinars163@gmail.com>
2023-07-05 10:40:57 +0400

    Создана таблица shopping_list_names

---
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/entities/ShoppingList.kt
@@ -0,0 +1,9 @@
+package com.ggr3ml1n.shoppinglist.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "shopping_list_names")
+data class ShoppingList(
+
+)
