048c44aa84c6a039527d9b89b93e75e1c9b4193f
Dinar Subeev <dinars163@gmail.com>
2023-07-05 11:42:14 +0400

    Заполнена таблица "shop_list_item"

--- app/src/main/java/com/ggr3ml1n/shoppinglist/entities/ShoppingListItem.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/entities/ShoppingListItem.kt
@@ -6,5 +6,21 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "shop_list_item")
 data class ShoppingListItem(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
 
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "item_info")
+    val itemInfo: String?,
+
+    @ColumnInfo(name = "item_checked")
+    val itemChecked: Int = 0,
+
+    @ColumnInfo(name = "list_id")
+    val listId: Int,
+
+    @ColumnInfo(name = "item_type")
+    val itemType: String = "item",
 )
