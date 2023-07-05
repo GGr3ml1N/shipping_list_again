06336e6f57870fdc4035dc8444559ef9457090e7
Dinar Subeev <dinars163@gmail.com>
2023-07-05 11:45:18 +0400

    Создана таблица "note_list"

---
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/entities/NoteItem.kt
@@ -0,0 +1,10 @@
+package com.ggr3ml1n.shoppinglist.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_list")
+data class NoteItem(
+
+)
