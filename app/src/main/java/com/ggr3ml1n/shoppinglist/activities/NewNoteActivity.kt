b2bde4149b838883af8f26ef44e089c297881a8a
Dinar Subeev <dinars163@gmail.com>
2023-07-06 23:58:07 +0400

    Создан новый activity, соответсвующая ему разметка и изменения в манифесте

---
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.ggr3ml1n.shoppinglist.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ggr3ml1n.shoppinglist.R
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
