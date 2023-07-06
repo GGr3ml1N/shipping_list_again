633985497a09902e28e2ccfb29999e61df53e845
Dinar Subeev <dinars163@gmail.com>
2023-07-06 18:14:46 +0400

    Прописана логика

--- app/src/main/java/com/ggr3ml1n/shoppinglist/fragments/FragmentManager.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/fragments/FragmentManager.kt
@@ -1,5 +1,15 @@
 package com.ggr3ml1n.shoppinglist.fragments
 
-object FragmentManager {
+import androidx.appcompat.app.AppCompatActivity
+import com.ggr3ml1n.shoppinglist.R
 
+object FragmentManager {
+    var currentFrag: BaseFragment? = null
+
+    fun setFragment(activity: AppCompatActivity, newFrag: BaseFragment) {
+        val transaction = activity.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.placeHolder, newFrag)
+        transaction.commit()
+        currentFrag = newFrag
+    }
 }
