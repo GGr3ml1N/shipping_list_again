d7b3652e0fbad05433addfcdfb5b7e38dbaeac8e
Dinar Subeev <dinars163@gmail.com>
2023-07-07 00:00:28 +0400

    Подключен binding

--- app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
@@ -1,12 +1,17 @@
 package com.ggr3ml1n.shoppinglist.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ggr3ml1n.shoppinglist.R
+import androidx.appcompat.app.AppCompatActivity
+import com.ggr3ml1n.shoppinglist.databinding.ActivityNewNoteBinding
 
 class NewNoteActivity : AppCompatActivity() {
+
+    private var _binding: ActivityNewNoteBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_note)
+        _binding = ActivityNewNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
