7c67ce5547e1e34ed1cfb19530a6cf39fbc87316
Dinar Subeev <dinars163@gmail.com>
2023-07-06 23:19:35 +0400

    Создан класс MainViewModel

---
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/vm/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.ggr3ml1n.shoppinglist.vm
+
+import androidx.lifecycle.ViewModel
+import com.ggr3ml1n.shoppinglist.db.MainDataBase
+
+class MainViewModel(database: MainDataBase) : ViewModel() {
+
+}
