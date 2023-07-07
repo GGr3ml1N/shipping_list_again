de10369e7436de268d2105b881e08d940db20a59
Dinar Subeev <dinars163@gmail.com>
2023-07-07 23:23:26 +0400

    Созданы функции получения реального времени и заполнения блокнота

--- app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
@@ -1,13 +1,17 @@
 package com.ggr3ml1n.shoppinglist.activities
 
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.ggr3ml1n.shoppinglist.R
 import com.ggr3ml1n.shoppinglist.databinding.ActivityNewNoteBinding
+import com.ggr3ml1n.shoppinglist.entities.NoteItem
 import com.ggr3ml1n.shoppinglist.fragments.NoteFragment
+import java.util.Locale
 
 class NewNoteActivity : AppCompatActivity() {
 
@@ -26,10 +30,11 @@ class NewNoteActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.save -> {
                 setMainResult()
             }
+
             android.R.id.home -> {
                 finish()
             }
@@ -37,13 +42,27 @@ class NewNoteActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setMainResult(){
+    private fun setMainResult() {
         val i = Intent().apply {
-            putExtra(NoteFragment.TITLE_KEY, binding.edTitle.text.toString())
-            putExtra(NoteFragment.DESC_KEY, binding.edDescription.text.toString())
+            putExtra(NoteFragment.NEW_NOTE_KEY, createNewNote())
         }
         setResult(RESULT_OK, i)
         finish()
+    }
+
+    private fun createNewNote(): NoteItem {
+        return NoteItem(
+            null,
+            binding.edTitle.text.toString(),
+            binding.edDescription.text.toString(),
+            getCurrentTime(),
+            ""
+        )
+    }
+
+    private fun getCurrentTime(): String {
+        val formater = SimpleDateFormat("hh:mm:ss - yyyy/MM/dd", Locale.getDefault())
+        return formater.format(Calendar.getInstance().time)
     }
 
     private fun actionBarSettings() {
