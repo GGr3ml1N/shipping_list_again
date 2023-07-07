a955d638ff260138680a60ee7462e346a6b39c2a
Dinar Subeev <dinars163@gmail.com>
2023-07-07 20:43:06 +0400

    Подключено editLauncher для обмена данными с NewNoteActivity

--- app/src/main/java/com/ggr3ml1n/shoppinglist/fragments/NoteFragment.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/fragments/NoteFragment.kt
@@ -1,10 +1,14 @@
 package com.ggr3ml1n.shoppinglist.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import com.ggr3ml1n.shoppinglist.activities.MainApp
 import com.ggr3ml1n.shoppinglist.activities.NewNoteActivity
@@ -16,12 +20,19 @@ class NoteFragment : BaseFragment() {
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>
+
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
 
     override fun onClickNew() {
-        startActivity(Intent(activity, NewNoteActivity::class.java))
+        editLauncher.launch(Intent(activity, NewNoteActivity::class.java))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        onEditResult()
     }
 
     override fun onCreateView(
@@ -32,8 +43,20 @@ class NoteFragment : BaseFragment() {
         return binding.root
     }
 
+    private fun onEditResult(){
+        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode != Activity.RESULT_OK) {
+                Log.d("MyLog", "title ${it.data?.getStringExtra(TITLE_KEY)}")
+                Log.d("MyLog", "description ${it.data?.getStringExtra(DESC_KEY)}")
+            }
+        }
+    }
+
     companion object {
-        @JvmStatic
+
+        const val TITLE_KEY = "title_key"
+        const val DESC_KEY = "desc_key"
+         @JvmStatic
         fun newInstance() = NoteFragment()
 
     }
