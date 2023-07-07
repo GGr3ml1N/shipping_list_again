3f3685826072d21291566b8766210db5e997d2af
Dinar Subeev <dinars163@gmail.com>
2023-07-07 20:53:09 +0400

    Функция setMainResult для обмена данными

--- app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
@@ -1,11 +1,13 @@
 package com.ggr3ml1n.shoppinglist.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.ggr3ml1n.shoppinglist.R
 import com.ggr3ml1n.shoppinglist.databinding.ActivityNewNoteBinding
+import com.ggr3ml1n.shoppinglist.fragments.NoteFragment
 
 class NewNoteActivity : AppCompatActivity() {
 
@@ -26,13 +28,22 @@ class NewNoteActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.save -> {
-                finish()
+                setMainResult()
             }
             android.R.id.home -> {
                 finish()
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMainResult(){
+        val i = Intent().apply {
+            putExtra(NoteFragment.TITLE_KEY, binding.edTitle.text.toString())
+            putExtra(NoteFragment.DESC_KEY, binding.edDescription.text.toString())
+        }
+        setResult(RESULT_OK, i)
+        finish()
     }
 
     private fun actionBarSettings() {
