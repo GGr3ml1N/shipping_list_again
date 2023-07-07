f81fee8786f998203db0ecafdc2cbea09115e0bb
Dinar Subeev <dinars163@gmail.com>
2023-07-07 20:22:14 +0400

    Выхов менюшки в NewNoteActivity

--- app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/activities/NewNoteActivity.kt
@@ -1,7 +1,10 @@
 package com.ggr3ml1n.shoppinglist.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.ggr3ml1n.shoppinglist.R
 import com.ggr3ml1n.shoppinglist.databinding.ActivityNewNoteBinding
 
 class NewNoteActivity : AppCompatActivity() {
@@ -13,5 +16,23 @@ class NewNoteActivity : AppCompatActivity() {
         _binding = ActivityNewNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.new_note_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.save -> {
+                finish()
+            }
+            android.R.id.home -> {
+                finish()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
