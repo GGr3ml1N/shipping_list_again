48788e8f8500a7574fd7fdadceccb42fe2d1cca3
Dinar Subeev <dinars163@gmail.com>
2023-07-26 18:56:38 +0400

    Подключено меню

--- app/src/main/java/com/ggr3ml1n/shoppinglist/activities/ShopListActivity.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/activities/ShopListActivity.kt
@@ -1,8 +1,11 @@
 package com.ggr3ml1n.shoppinglist.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.ggr3ml1n.shoppinglist.R
 import com.ggr3ml1n.shoppinglist.databinding.ActivityShopListBinding
 import com.ggr3ml1n.shoppinglist.entities.ShopListNameItem
 import com.ggr3ml1n.shoppinglist.utils.Extension
@@ -12,6 +15,8 @@ class ShopListActivity : AppCompatActivity() {
 
     private var _binding: ActivityShopListBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var saveItem: MenuItem
 
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModel.MainViewModelFactory((applicationContext as MainApp).database)
@@ -29,6 +34,30 @@ class ShopListActivity : AppCompatActivity() {
     private fun init() {
         shopListName = Extension.getSerializable(intent, SHOP_LIST_NAME, ShopListNameItem::class.java)
         binding.tvTest.text = shopListName?.name
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.shop_list_menu, menu)
+        saveItem = menu?.findItem(R.id.save_item)!!
+        val newItem = menu.findItem(R.id.new_item)
+        newItem.setOnActionExpandListener(expandActionView())
+        saveItem.isVisible = false
+        return true
+    }
+
+    private fun expandActionView(): MenuItem.OnActionExpandListener {
+        return object: MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
+                saveItem.isVisible = true
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+                saveItem.isVisible = false
+                invalidateOptionsMenu()
+                return true
+            }
+        }
     }
 
     companion object {
