b1f3ee06afd76dee5b409577febef0787e5c2e60
Dinar Subeev <dinars163@gmail.com>
2023-07-28 09:43:06 +0400

    Инициализирован rcView,   добавлен observer

--- app/src/main/java/com/ggr3ml1n/shoppinglist/activities/ShopListActivity.kt
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/activities/ShopListActivity.kt
@@ -3,10 +3,15 @@ package com.ggr3ml1n.shoppinglist.activities
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ggr3ml1n.shoppinglist.R
+import com.ggr3ml1n.shoppinglist.adapters.ShopListItemAdapter
 import com.ggr3ml1n.shoppinglist.databinding.ActivityShopListBinding
+import com.ggr3ml1n.shoppinglist.entities.ShopListItem
 import com.ggr3ml1n.shoppinglist.entities.ShopListNameItem
 import com.ggr3ml1n.shoppinglist.utils.Extension
 import com.ggr3ml1n.shoppinglist.vm.MainViewModel
@@ -17,6 +22,8 @@ class ShopListActivity : AppCompatActivity() {
     private val binding get() = _binding!!
 
     private lateinit var saveItem: MenuItem
+    private var edText: EditText? = null
+    private var adapter: ShopListItemAdapter? = null
 
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModel.MainViewModelFactory((applicationContext as MainApp).database)
@@ -29,24 +36,34 @@ class ShopListActivity : AppCompatActivity() {
         _binding = ActivityShopListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
+        observer()
+        initRcView()
     }
 
     private fun init() {
-        shopListName = Extension.getSerializable(intent, SHOP_LIST_NAME, ShopListNameItem::class.java)
-        binding.tvTest.text = shopListName?.name
+        shopListName =
+            Extension.getSerializable(intent, SHOP_LIST_NAME, ShopListNameItem::class.java)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.shop_list_menu, menu)
         saveItem = menu?.findItem(R.id.save_item)!!
         val newItem = menu.findItem(R.id.new_item)
+        edText = newItem.actionView?.findViewById(R.id.edNewShopItem) as EditText
         newItem.setOnActionExpandListener(expandActionView())
         saveItem.isVisible = false
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.save_item) {
+            addNewShopItem()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun expandActionView(): MenuItem.OnActionExpandListener {
-        return object: MenuItem.OnActionExpandListener {
+        return object : MenuItem.OnActionExpandListener {
             override fun onMenuItemActionExpand(item: MenuItem): Boolean {
                 saveItem.isVisible = true
                 return true
@@ -58,6 +75,46 @@ class ShopListActivity : AppCompatActivity() {
                 return true
             }
         }
+    }
+
+    private fun addNewShopItem() {
+        if (edText?.text.toString().isEmpty()) return
+        val item = ShopListItem(
+            null,
+            edText?.text.toString(),
+            null,
+            0,
+            shopListName?.id!!,
+            0,
+        )
+        edText?.setText("")
+        mainViewModel.insertShopListItem(item)
+    }
+
+    private fun observer() {
+        mainViewModel.getAllItemsFromList(shopListName?.id!!).observe(this) {
+            adapter?.submitList(it)
+            binding.tvEmpty.visibility = if(it.isEmpty()) View.VISIBLE else View.GONE
+        }
+    }
+
+    private fun initRcView() = with(binding) {
+        adapter = ShopListItemAdapter(object: ShopListItemAdapter.Listener {
+            override fun deleteItem(id: Int) {
+
+            }
+
+            override fun onClickItem(shopListName: ShopListNameItem) {
+
+            }
+
+            override fun editItem(shopListName: ShopListNameItem) {
+
+            }
+
+        })
+        rcView.layoutManager = LinearLayoutManager(this@ShopListActivity)
+        rcView.adapter = adapter
     }
 
     companion object {
